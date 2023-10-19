c7ae66d16ff3ccca48b21ad5b3f74af4aa94740c
VitorDD <vitordevila@gmail.com>
2023-10-19 20:01:52 -0300

    commit 1910

---
+++ PSIU/PSIUWeb/Models/Psico.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PSIUWeb.Models
+{
+    public class Psico<T>
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CRP { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public T Aguinaldo { get; set; }
+    }
+}
