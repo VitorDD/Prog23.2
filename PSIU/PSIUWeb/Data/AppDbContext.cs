8691c28a4e9c9768dc9ef2e1d0322c71ff8766a3
VitorDD <vitordevila@gmail.com>
2023-11-08 19:55:15 -0300

    inicio da aula 08/11

--- PSIU/PSIUWeb/Data/AppDbContext.cs
+++ PSIU/PSIUWeb/Data/AppDbContext.cs
@@ -5,19 +5,22 @@ using PSIUWeb.Models;
 
 namespace PSIUWeb.Data
 {
-    public class AppDbContext : 
+    public class AppDbContext :
         IdentityDbContext<AppUser>
     {
-        public AppDbContext(DbContextOptions options) 
+        public AppDbContext(DbContextOptions options)
             : base(options)
         {
         }
 
         public DbSet<Pacient>? Pacients { get; set; }
+
         public DbSet<Psico>? Psicos { get; set; }
+
         public DbSet<Category>? Categories { get; set; }
+
         public DbSet<Content>? Contents { get; set; }
-        public DbSet<ContentCategory>? ContentCategories { get; set; }
+        public DbSet <ContentCategory>? ContentCategories { get; set; }
 
     }
 }
--- PSIU/PSIUWeb/Data/SeedData.cs
+++ PSIU/PSIUWeb/Data/SeedData.cs
@@ -42,6 +42,34 @@ namespace PSIUWeb.Data
 
                 context.SaveChanges();
             }
+
+            if (!context.Psicos.Any())
+            {
+                context.Psicos.AddRange(
+                    new Psico
+                    {
+                        Name = "Mauricio",
+                        BirthDate = new DateTime(1984, 7, 5),
+                        Race = Race.Pardo,
+                        Crp = 18098765,
+                        Cep = 88998000,
+                        Liberado = true
+                    },
+                    new Psico
+                    {
+                        Name = "Marcos",
+                        BirthDate = new DateTime(1987, 2, 28),
+                        Race = Race.Pardo,
+                        Crp = 12345678,
+                        Cep = 89670000,
+                        Liberado = true
+
+                    }
+                );
+
+                context.SaveChanges();
+            }
+
         }
 
     }
